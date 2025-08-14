c151364c1861a20f1198a909ebbf50a893499821
ireka97 <reksuu.official@gmail.com>
2025-08-15 02:28:08 +0700

    Update map.js

--- src/config/map.js
+++ src/config/map.js
@@ -3,20 +3,22 @@ const MAP_LAYERS = {
   name: "OpenStreetMap",
   url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
   attribution: "© MyTracking App 2025",
-  thumbnail: "/public/mapthumbnail/osm.png"
+  thumbnail: "https://openmaptiles.org/img/styles/openmaptiles.png"
  },
  satellite: {
   name: "Esri Satellite",
   url: "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
   attribution: "© MyTracking App 2025",
-  thumbnail: "/public/mapthumbnail/citra.png"
+  thumbnail:
+   "https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEg3Ya_I4fQvPPKYNbbKQ5UpI6dbFPwD4y-Ch8lYHynurWnrSJc6rEqRSGtNziNvbJPBmSk36ocZLZkeHk4njmaaFFobZBv7IoIreWmHCulKzl0xNd3m0-Vygw1Imo9Che2SPx6Qo_mnl88/s1600/MONAS.jpg"
  },
  OpenTopo: {
   name: "Carto Topo",
   url: "https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png",
   attribution: "© MyTracking App 2025",
-  thumbnail: "/public/mapthumbnail/topo.png"
+  thumbnail: "https://upload.wikimedia.org/wikipedia/commons/3/3d/OpenTopomap_Meiningen.jpg"
  }
 }
 
 export { MAP_LAYERS };
+           
