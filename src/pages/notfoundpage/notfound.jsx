6e6684c3ca0d8e8ff0d6c2908e3adb3eac3f69f5
ireka97 <reksuu.official@gmail.com>
2025-08-15 02:45:03 +0700

    edit css

--- src/pages/notfoundpage/notfound.jsx
+++ src/pages/notfoundpage/notfound.jsx
@@ -3,7 +3,7 @@ import {useNavigate} from "react-router-dom"
 import "./notfound.css"
 
 export default function NotFound() {
- const navigate = useNavigate()
+//  const navigate = useNavigate()
 
  return (
   <div className="notfound-container">
