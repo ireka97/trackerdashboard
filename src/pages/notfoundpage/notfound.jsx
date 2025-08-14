76836cb024e12cc086d88dfdfbfa8e84d9d61c6e
ireka97 <reksuu.official@gmail.com>
2025-08-14 22:12:42 +0700

    ad vercel

--- src/pages/notfoundpage/notfound.jsx
+++ src/pages/notfoundpage/notfound.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import {useNavigate} from "react-router-dom"
-import "./NotFound.css"
+import "./notfound.css"
 
 export default function NotFound() {
  const navigate = useNavigate()
